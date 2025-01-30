980ea0c02108eaa6938f0593c840c9c7e1c248f5
JUAN CARLOS DELGADILLO SALAZAR <1152249@horusur.corp>
2025-01-30 15:56:52 -0600

    18

--- main.tf
+++ main.tf
@@ -1,15 +1,9 @@
-variable "ambiente" {
-  description = "ambiente en el que se va a desplegar el ambiente"
-  type = string
-}
-
 module "s3_bucket_account_a" {
   source      = "./s3-repo" # Ruta al módulo
   providers   = { aws = aws.account_a }
   bucket_name = var.bucket_names["account_a"]
   environment = var.environments["env_a"]
-  ambiente    = var.ambiente
-  count       = ambiente == "account_a" ? 1 : 0  # Se ejecuta solo si "ambiente" es "account_a"
+  count       = var.ambiente == "account_a" ? 1 : 0  # Se ejecuta solo si "ambiente" es "account_a"
 }
 
 module "s3_bucket_account_b" {
@@ -17,6 +11,5 @@ module "s3_bucket_account_b" {
   providers   = { aws = aws.account_b }
   bucket_name = var.bucket_names["account_b"]
   environment = var.environments["env_b"]
-  ambiente    = var.ambiente
-  count       = ambiente == "account_b" ? 1 : 0  # Se ejecuta solo si "a" es "account_b"
+  count       = var.ambiente == "account_b" ? 1 : 0  # Se ejecuta solo si "a" es "account_b"
 }
