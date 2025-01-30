bff3d9f70264e9c5e75bd16f912b86956a43b7c5
JUAN CARLOS DELGADILLO SALAZAR <1152249@horusur.corp>
2025-01-30 12:09:15 -0600

    v4

--- providers.tf
+++ providers.tf
@@ -6,3 +6,12 @@ provider "aws" {
     role_arn = "arn:aws:iam::222634362165:role/terraform-module-role"
   }
 }
+
+provider "aws" {
+  alias  = "account_b"
+  region = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::204326978364:role/terraform-module-role"
+  }
+}
