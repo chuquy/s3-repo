aa5f331ffcff0ae795ca2dd7225f2d6cea57cfb2
JUAN CARLOS DELGADILLO SALAZAR <1152249@horusur.corp>
2025-01-30 15:19:26 -0600

    v14

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "s3_bucket_account_a" {
   providers   = { aws = aws.account_a }
   bucket_name = var.bucket_names["account_a"]
   environment = var.environments["env_a"]
-  count       = var.ambiente == "account_a" ? 1 : 0  # Se ejecuta solo si "ambiente" es "account_a"
+  count       = ambiente == "account_a" ? 1 : 0  # Se ejecuta solo si "ambiente" es "account_a"
 }
 
 module "s3_bucket_account_b" {
@@ -16,5 +16,5 @@ module "s3_bucket_account_b" {
   providers   = { aws = aws.account_b }
   bucket_name = var.bucket_names["account_b"]
   environment = var.environments["env_b"]
-  count       = var.ambiente == "account_b" ? 1 : 0  # Se ejecuta solo si "a" es "account_b"
+  count       = ambiente == "account_b" ? 1 : 0  # Se ejecuta solo si "a" es "account_b"
 }
