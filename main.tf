aefdb56214fa30d6da970b3e5ba3f116dc193869
JUAN CARLOS DELGADILLO SALAZAR <1152249@horusur.corp>
2025-01-30 13:16:44 -0600

    v11

--- main.tf
+++ main.tf
@@ -1,8 +1,14 @@
-module "s3_buckets" {
-  for_each = local.accounts
-
+module "s3_bucket_account_a" {
   source      = "./s3-repo" # Ruta al módulo
-  providers   = { aws = each.value.provider }
-  bucket_name = each.value.bucket_name
-  environment = each.value.environment
+  providers   = { aws = aws.account_a }
+  bucket_name = var.bucket_names["account_a"]
+  environment = var.environments["env_a"]
+  count = 5
 }
+
+# module "s3_bucket_account_b" {
+#   source      = "./s3-repo" # Ruta al módulo
+#   providers   = { aws = aws.account_b }
+#   bucket_name = var.bucket_names["account_b"]
+#   environment = var.environments["env_b"]
+# }
